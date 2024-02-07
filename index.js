ad0b99b726d911c73cbfe999a11bf0155972ce96
TatianaChepovaya <79250983+TatianaChepovaya@users.noreply.github.com>
2024-02-07 15:00:02 +0200

    slider

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
   $(".product__slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 5,
+    slidesToScroll: 5,
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true,
+    nextArrow: $('.line__btn-right-arrow'),
+    prevArrow: $('.line__btn-left-arrow'),
   });
 });
