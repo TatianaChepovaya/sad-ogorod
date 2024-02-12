e747d56ded73234cb1f6e12b90cc1457ce6eb692
TatianaChepovaya <79250983+TatianaChepovaya@users.noreply.github.com>
2024-02-12 11:12:11 +0200

    mob

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ $(document).ready(function () {
     event.preventDefault();
     $(this).toggleClass('active');
   })
+   if ($(window).width() < 768) {
+    $('.login').appendTo('.footer__login-container')
+  }
+  if ($(window).width() < 768) {
+    $('.menu__desk').appendTo('.footer__mob-menu-container')
+  }
+  $('.footer__mob-menu-container').on('click',function(event){
+    event.preventDefault();
+    $(this).toggleClass('active');
+  })
+  $('.footer__login-container').on('click',function(event){
+    event.preventDefault();
+    $(this).toggleClass('active');
+  })
+  if ($(window).width() < 768) {
+    $('.login__container').appendTo('.footer__login-container')
+  }
 });
+
+
