8ff44d49ec341630ac265870dfde9d53ded2089f
TatianaChepovaya <79250983+TatianaChepovaya@users.noreply.github.com>
2024-02-20 09:31:18 +0200

    phone

--- index.js
+++ index.js
@@ -10,27 +10,61 @@ $(document).ready(function () {
     variableWidth: true,
 
   });
+
   $('.catalog__item').on('click',function(event){
     event.preventDefault();
     $(this).toggleClass('active');
   })
+
    if ($(window).width() < 768) {
     $('.login').appendTo('.footer__login-container')
   }
+
   if ($(window).width() < 768) {
     $('.menu__desk').appendTo('.footer__mob-menu-container')
   }
+
   $('.footer__mob-menu-container').on('click',function(event){
     event.preventDefault();
     $(this).toggleClass('active');
   })
+
   $('.footer__login-container').on('click',function(event){
     event.preventDefault();
     $(this).toggleClass('active');
   })
+
   if ($(window).width() < 768) {
     $('.login__container').appendTo('.footer__login-container')
   }
+  $('.btn__phone').on('click',function(event){
+    event.preventDefault();
+    $('.phone__list').toggleClass('active');
+    $('.phone__container').toggleClass('active');
+  })
+  $('.phone__list').on('click',function(event){
+    event.preventDefault();
+    $(".contact__form").toggleClass('active');
+  })
+  $('.login__part').on('click',function(event){
+    event.preventDefault();
+    $(this).toggleClass('active');
+  })
+  $('.btn__search').on('click',function(event){
+    event.preventDefault();
+    $('.search').toggleClass('active');
+    $('.search__container').toggleClass('active');
+  })
+  $('.btn__menu').on('click',function(event){
+    event.preventDefault();
+    $(".mob__menu").toggleClass('active');
+    $(".mob__menu-container").toggleClass('active');
+  })
+  $('.mob__menu-item').on('click',function(event){
+    event.preventDefault();
+    $(this).toggleClass('active');
+
+  })
 });
 
 
